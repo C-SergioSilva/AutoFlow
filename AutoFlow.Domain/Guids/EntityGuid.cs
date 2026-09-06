816578efaf7b4d505888284a51f0630e52ec00ac
CSilva <carlos.silva@tty2000.com.br>
2026-09-05 21:19:34 -0300

    Add project files.

---
+++ AutoFlow.Domain/Guids/EntityGuid.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoFlow.Domain.Guids
+{
+    public class EntityGuid
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+
+        public bool Deleted { get; set; } = false;
+    }
+}
---
+++ AutoFlow.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoFlow.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
