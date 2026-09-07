d781f9480c9e38bf6943dc1854be35df0bb04663
CSilva <carlos.silva@tty2000.com.br>
2026-09-07 12:49:30 -0300

    Iniciando implementaçoes front e back

---
+++ AutoFlow.UI/API/ClienteApiController.cs
@@ -0,0 +1,22 @@
+﻿using AutoFlow.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClienteApiController : ControllerBase // ou ClienteApiController
+{
+    private readonly IClienteService _clienteService;
+
+    public ClienteApiController(IClienteService clienteService)
+    {
+        _clienteService = clienteService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ObterTodos()
+    {
+        // Aqui o serviço busca no banco e a API devolve o JSON puro
+        var lista = await _clienteService.ObterTodos();
+        return Ok(lista);
+    }
+}
---
+++ AutoFlow.UI/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoFlow.UI.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
