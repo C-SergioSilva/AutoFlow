a11ed550192e523a140353d560e6f2c4f62d0ba6
CSilva <carlos.silva@tty2000.com.br>
2026-09-07 16:47:04 -0300

    Organizando o código front-end

---
+++ AutoFlow.UI/wwwroot/js/cliente/cliente-index.js
@@ -0,0 +1,35 @@
+﻿
+// Assim que a janela carregar, chamamos nossa função para buscar os dados
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+        // Fazendo uma requisição GET para a nossa API
+        // (Substitua 'clienteapi' pelo nome exato da rota da sua controladora de API)
+        const resposta = await fetch('/api/clienteapi');
+
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar os dados da API.");
+        }
+
+        const clientes = await resposta.json();
+        var doc = document;
+        const tbody = doc.getElementById("tabela-clientes");
+
+        // Limpa a tabela caso tenha algo
+        tbody.innerHTML = "";
+
+        // Percorre cada cliente que veio da API e cria uma linha na tabela
+        clientes.forEach(cliente => {
+            const tr = doc.createElement("tr");
+            tr.innerHTML = `
+                <td>${cliente.nome}</td>
+                <td>${cliente.telefoneWhatsApp}</td>
+                <td>${cliente.documento}</td>
+                            `;
+            tbody.appendChild(tr);
+        });
+
+    } catch (error) {
+        console.error("Erro:", error);
+        alert("Não foi possível carregar a lista de clientes.");
+    }
+});
